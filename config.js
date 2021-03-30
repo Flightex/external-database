e5b889bb1844857f508f95e26acc5644a80dbfbf
Airbus A330-200 <united.vietnam.airlines@gmail.com>
2021-03-30 15:33:43 +0000

    Update config.js comment design

--- config.js
+++ config.js
@@ -5,10 +5,9 @@
  | (_| (_) | | | |  _| | (_| |_ | \__ \
   \___\___/|_| |_|_| |_|\__, (_)/ |___/
                         |___/ |__/     
-*/
 
-/*
   The config.js is essential for starting the server.  One has already been set up for you below, and we recommend you change the values as you wish (especially the token as the current one is not secure).
+  
 */
 
 module.exports = {
